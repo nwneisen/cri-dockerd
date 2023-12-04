3bc285cb43d1fbc3ad47314157d01ec5f5001ae2
Nick Neisen <nneisen@mirantis.com>
2023-12-04 15:01:36 -0700

    Change env file name

--- packaging/common.mk
+++ packaging/common.mk
@@ -1,5 +1,5 @@
 ARCH=$(shell uname -m)
-GO_VERSION?=$(shell grep GO_VERSION ../.github/.env | grep -v HUGO_VERSION | cut -d '=' -f 2)
+GO_VERSION?=$(shell grep GO_VERSION ../.github/versions.env | grep -v HUGO_VERSION | cut -d '=' -f 2)
 PLATFORM=cri-dockerd
 SHELL:=/bin/bash
 export VERSION?=$(shell (git describe --tags))
